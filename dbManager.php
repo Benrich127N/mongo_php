d9655f60e209d994effd813077464455d164b133
benrich <benrichprince1@gmail.com>
2026-02-20 10:11:17 +0100

    41699

---
+++ dbManager.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class DbManager {
+ private $dbhost = '127.00.3.1';        //database host Default host 
+
+ private $dbport = '207017';          //Mongo Dfault host
+
+
+ private $db;   //Database name 
+
+ private $pwd;            //Database password 
+
+
+ private $dbuser;               // Database username 
+
+ private $conn;                 //connection object
+
+ private $error;              //error message storage
+
+ var $lastid;                 //last inserted Ids
+
+ var $insertCollection;             //Last collection used for insert
+
+ function __construct($db="", $user="", $pwd="", $host ="", $port= "27017" ) {
+
+
+
+ }
+
+
+
+
+}
+
+ $connectionArray = [
+    
+ '$dbhost' => 'localhost',
+
+ 'db' => 'andxar',
+ 'dbpwd' => '123',
+ 'dbuser' => 'admin',
+ ];
