bffb1aa217d2acb2e532c87e3f5e5ea1b7c40692
benrich <benrichprince1@gmail.com>
2026-02-20 10:57:36 +0100

    41699

--- qry_unique_and_ordinary_index_way_person_disgrace_me for.php
+++ qry_unique_and_ordinary_index_way_person_disgrace_me for.php
@@ -5,3 +5,16 @@ $db->qry("create", "users", ["unique" => ["email", "username"],
 "ordinary" => ["lastname", "status"], 
 
 ]);
+
+
+
+
+$userData = [
+  "email" => "benrich@gmail.com",
+  "password" => password_hash("password", PASSWORD_DEFAULT),
+  "firstname" => "John",
+
+
+];
+
+//in an array you can pass an array to add multiple inputs and objects to nice oh 
