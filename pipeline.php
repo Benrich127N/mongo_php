c0717a3bc53c359b68b959fcf0154e0a3de70509
benrich <benrichprince1@gmail.com>
2026-02-21 11:43:54 +0100

    41699

--- pipeline.php
+++ pipeline.php
@@ -10,3 +10,19 @@ $pipeline = [
 
 
 $cursor = $collection->aggregate($pipeline);
+
+
+//sort the from a-z asending order then limits it to 10 
+
+
+
+$pipeline = [
+'limit' => [10],
+'sort' => ['firstname' => -1]
+
+];
+
+$cusor =$collection->aggregate($pipeline);
+
+
+// this grabs to random records and sorts by ascending order and this wont be accurate
