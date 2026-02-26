3cb2a4f9234ff113c6d15745ada5322a3b8206e2
benrich <benrichprince1@gmail.com>
2026-02-26 15:48:44 +0100

    42705

--- glance.php
+++ glance.php
@@ -7,8 +7,22 @@ public function Update() {
 
 
 
-//get 
+//get user id from post 
 $request = $_POST['order_id'];
+
+//step 2 : Define validation 
+
+$data = [
+
+'order_name' => 'required|min_len 3',
+'order_type' => 'required|min_len 3',
+'assinged_to' => 'required',
+'comment' => 'required|min_len',
+
+];
+
+
+
 
 
 
